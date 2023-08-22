b86f33fb08ad4c7461caff3dabdc7f7007cfcd8f
miiwnoii <jutamas.kanthong@gmail.com>
2023-08-22 15:41:43 +0700

    update photo

--- src/components/Agenda.tsx
+++ src/components/Agenda.tsx
@@ -7,7 +7,7 @@ export default function Agenda() {
           <li>
             <div class="flex-start flex items-center pt-6">
               <div class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-400 dark:bg-neutral-500"></div>
-              <p class="text-neutral-500 dark:text-neutral-300">8:29 AM</p>
+              <p class="text-neutral-600 dark:text-neutral-300">8:29 AM</p>
             </div>
             <div class="mb-6 ml-4 mt-2">
               <h4 class="mb-1.5 font-semibold">พิธีแห่ขันหมาก</h4>
@@ -16,7 +16,7 @@ export default function Agenda() {
           <li>
             <div class="flex-start flex items-center pt-2">
               <div class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-400 dark:bg-neutral-500"></div>
-              <p class="text-neutral-500 dark:text-neutral-300">9:09 AM</p>
+              <p class="text-neutral-600 dark:text-neutral-300">9:09 AM</p>
             </div>
             <div class="mb-6 ml-4 mt-2">
               <h4 class="mb-1.5 font-semibold">พิธีหมั้น และรับไหว้</h4>
@@ -25,7 +25,7 @@ export default function Agenda() {
           <li>
             <div class="flex-start flex items-center pt-2">
               <div class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-400 dark:bg-neutral-500"></div>
-              <p class="text-neutral-500 dark:text-neutral-300">10.00 AM</p>
+              <p class="text-neutral-600 dark:text-neutral-300">10.00 AM</p>
             </div>
             <div class="ml-4 mt-2 pb-5">
               <h4 class="mb-1.5 font-semibold">พิธีหลั่งน้ำพระพุทธมนต์</h4>
@@ -34,7 +34,7 @@ export default function Agenda() {
           <li>
             <div class="flex-start flex items-center pt-2">
               <div class="-ml-[5px] mr-3 h-[9px] w-[9px] rounded-full bg-neutral-400 dark:bg-neutral-500"></div>
-              <p class="text-neutral-500 dark:text-neutral-300">11.00 AM</p>
+              <p class="text-neutral-600 dark:text-neutral-300">11.00 AM</p>
             </div>
             <div class="ml-4 mt-2 pb-2">
               <h4 class="mb-1.5 font-semibold">ฉลองมงคลสมรส(โต๊ะจีน)</h4>
